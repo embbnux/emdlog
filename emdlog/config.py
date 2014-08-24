d522e0ded601f8e11c6d7939abfd1c4e2d058984
emrpi <embbnux@embbnux.com>
2014-08-24 10:05:37 +0000

    first

---
+++ emdlog/config.py
@@ -0,0 +1,24 @@
+# welcome to my blog : Blog of Embbnux 
+# url:http://www.embbnux.com/ 
+# author : Embbnux Ji`
+
+#configuration
+import os
+
+class Config(object):
+	BASEDIR = os.path.abspath(os.path.dirname(__file__))
+	DATABASE = BASEDIR+'/db/blog.db'
+	SECRET_KEY = os.urandom(24)
+	USERNAME = 'admin'
+	PASSWORD = 'default'
+	DEBUG = True
+	HOST = '0.0.0.0'
+	PORT = 2000
+
+class Development(Config):
+	pass
+
+class Production(Config):
+	DEBUG = False
+	HOST = '0.0.0.0'
+	PORT = 8080
---
+++ emdlog/views.py
@@ -0,0 +1,93 @@
+# welcome to my blog : Blog of Embbnux 
+# url:http://www.embbnux.com/ 
+# author : Embbnux Ji
+
+# all the imports
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from contextlib import closing
+from emdlog import app
+import re
+
+@app.route('/')
+def show_entries():
+	cur = g.db.execute('select id, title, text from entries order by id desc')
+	entries = [dict(id=row[0],title=row[1], text=row[2]) for row in cur.fetchall()]
+	for entry in entries:
+		if(len(entry['text'])>20):
+			entry['text'] = entry['text'][0:19]
+	return render_template('show_entries.html', entries=entries)
+
+@app.route('/node/<int:node_id>')
+def show_node(node_id):
+	cur = g.db.execute('select id,title,text from entries where id= ? ',[node_id])
+	node = [dict(id=row[0],title=row[1], text=row[2]) for row in cur.fetchall()]
+	r = re.compile('<br>')
+	text= r.sub('/r/n',node[0]['text'])
+	node[0]['text']= text
+	return render_template('show_node.html',node=node[0])
+
+@app.route('/add', methods=['GET','POST'])
+def add_entry():
+	if not session.get('logged_in'):
+		abort(401)
+	if request.method == 'POST':
+		text= request.form['text']
+		g.db.execute('insert into entries (title, text) values (?, ?)',
+				[request.form['title'], text])
+		g.db.commit()
+
+		flash('New entry was successfully posted')
+		return redirect(url_for('show_entries'))
+	elif request.method == 'GET':
+		return render_template('add_node.html')
+	return redirect(url_for('show_entries'))
+
+@app.route('/edit/<int:node_id>',methods=['GET','POST'])
+def edit_entry(node_id):
+	if not session.get('logged_in'):
+		abort(401)
+	if request.method == 'GET':
+		cur = g.db.execute('select id,title,text from entries where id= ? ',
+				[node_id])
+		node = [dict(id=row[0],title=row[1], text=row[2]) for row in cur.fetchall()]
+		return render_template('edit_node.html',node=node[0])
+	elif request.method == 'POST':
+		text = request.form['text']
+		node_id = request.form['node_id']
+		g.db.execute('update entries set title= ? , text=? where id=?',
+				[request.form['title'],text,node_id])
+		g.db.commit()
+
+		flash('The article has been updated.')
+		return redirect(url_for('show_node'),node_id=node_id)
+	return redirect(url_for('show_entries'))
+@app.route('/delete',methods=['POST'])
+def delete_entry():
+	if not session.get('logged_in'):
+		abort(401)
+	node_id = request.args.get('node_id','')
+	g.db.execute('delete from entries where id = ?',node_id)
+	g.db.commit()
+	flash('The article has been deleted')
+	return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid password'
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return redirect(url_for('show_entries'))
+	return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('show_entries'))
+
